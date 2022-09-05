cd9b50cec86ff5d00fafde36125f016007e45727
buzhiyun <buzhiyun@sina.com>
2022-09-05 14:35:12 +0800

    修复 command handler arg叠加问题

--- handler/command_handler.go
+++ handler/command_handler.go
@@ -10,15 +10,14 @@ import (
 */
 type commandHandler struct {
 	isAvailable bool
-	name 		string
-	cmdList		[]cmd
+	name        string
+	cmdList     []cmd
 }
-
 
 /*
 name 参数 第一个是 handle名字 ， 第二个是 是否windows文件 "true"|"false"
 */
-func (h *commandHandler)NewHandler(name string,cmdList []cmd,args... string) handler {
+func (h *commandHandler) NewHandler(name string, cmdList []cmd, args ...string) handler {
 	h.name = name
 	h.cmdList = cmdList
 	h.isAvailable = true
@@ -28,20 +27,21 @@ func (h *commandHandler)NewHandler(name string,cmdList []cmd,args... string) han
 	return h
 }
 
-func (h *commandHandler)String() string {
+func (h *commandHandler) String() string {
 	return h.name
 }
 
-func (h *commandHandler)Available() bool {
+func (h *commandHandler) Available() bool {
 	return h.isAvailable
 }
 
-func (h *commandHandler)UpdateHost(domain,ip string) (err error) {
+func (h *commandHandler) UpdateHost(domain, ip string) (err error) {
 	for _, cmd := range h.cmdList {
-		args := cmd.Args
-		args[len(args) - 1] = args[len(args) - 1] + " -domain " + domain + " -ip " + ip
-		result := utils.Exec(cmd.Command,args...)
-		golog.Infof("执行cmd：\n%s",result)
+		args := make([]string, len(cmd.Args))
+		copy(args, cmd.Args)
+		args[len(args)-1] = args[len(args)-1] + " -domain " + domain + " -ip " + ip
+		result := utils.Exec(cmd.Command, args...)
+		golog.Infof("执行cmd：\n%s", result)
 	}
 	return
 }
